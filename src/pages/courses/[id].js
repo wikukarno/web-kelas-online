042a9183ae6d975e91199f6730d384f3ba0c6145
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2021-09-24 22:25:55 +0700

    update file & add folder Details/Feature.js

--- src/pages/courses/[id].js
+++ src/pages/courses/[id].js
@@ -4,6 +4,11 @@ import courses from "src/constants/api/courses";
 import Youtube from "react-youtube";
 import Header from "src/parts/Header";
 
+import Nametag from "public/images/icon-nametag.svg";
+import Playback from "public/images/icon-playback.svg";
+import Certificate from "public/images/icon-certificate.svg";
+import Feature from "src/parts/Details/Fetaure";
+
 function DetailsCourse({ data }) {
   console.log(data);
   return (
@@ -45,6 +50,36 @@ function DetailsCourse({ data }) {
         </div>
         <div className='container mx-auto z-10 relative'>
           <Header></Header>
+        </div>
+      </section>
+
+      <section className='container mx-auto pt-24 relative'>
+        <div className='absolute top-0 w-full transform -translate-y-1/2'>
+          <div className='w-3/4 mx-auto'>
+            <div className='flex justify-between'>
+              <Feature
+                data={{
+                  icon: <Nametag className='fill-teal-500' />,
+                  meta: "Student",
+                  value: data?.total_student ?? 0,
+                }}
+              />
+              <Feature
+                data={{
+                  icon: <Playback className='fill-teal-500' />,
+                  meta: "Video",
+                  value: data?.total_videos ?? 0,
+                }}
+              />
+              <Feature
+                data={{
+                  icon: <Certificate className='fill-teal-500' />,
+                  meta: "Certificate",
+                  value: data?.certificate === 1 ? "Tersedia" : "-",
+                }}
+              />
+            </div>
+          </div>
         </div>
       </section>
     </>
