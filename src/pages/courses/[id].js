54d9a0f8d2912bf403850293112514fbb54e26bd
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2021-09-25 14:19:09 +0700

    update file & add folder

--- src/pages/courses/[id].js
+++ src/pages/courses/[id].js
@@ -1,5 +1,8 @@
+import { useEffect, useRef, useState } from "react";
 import Head from "next/head";
 import courses from "src/constants/api/courses";
+import { CSSTransition } from "react-transition-group";
+import formatThousand from "src/helpers/formatThousand";
 
 import Youtube from "react-youtube";
 import Header from "src/parts/Header";
@@ -8,9 +11,26 @@ import Nametag from "public/images/icon-nametag.svg";
 import Playback from "public/images/icon-playback.svg";
 import Certificate from "public/images/icon-certificate.svg";
 import Feature from "src/parts/Details/Fetaure";
+import CoursePhoto from "src/parts/Details/CoursePhoto";
+import Footer from "src/parts/Footer";
 
 function DetailsCourse({ data }) {
   console.log(data);
+
+  const footer = useRef(null);
+  const [isSticky, setisSticky] = useState(() => true);
+
+  useEffect(() => {
+    const stickyOffsetTop = footer.current.getBoundingClientRect().top;
+    const stickyMetaToggler = () => {
+      setisSticky(stickyOffsetTop >= window.pageYOffset + window.innerHeight);
+    };
+
+    window.addEventListener("scroll", stickyMetaToggler);
+    return () => {
+      window.removeEventListener("scroll", stickyMetaToggler);
+    };
+  }, []);
   return (
     <>
       <Head>
@@ -81,6 +101,70 @@ function DetailsCourse({ data }) {
             </div>
           </div>
         </div>
+        <div>
+          <CSSTransition
+            in={isSticky}
+            timeout={300}
+            classNames='meta-price'
+            unmountOnExit>
+            <div className='meta-price w-full bg-white z-50 left-0 py-3'>
+              <div className='w-3/4 mx-auto'>
+                <div className='flex items-center'>
+                  <div className='w-full'>
+                    <h2 className='text-gray-600'>Nama Kelas</h2>
+                    <h3 className='text-2xl text-gray-900'>
+                      {data?.name ?? "Nama Kelas"}
+                    </h3>
+                  </div>
+                  <h5 className='text-2xl text-teal-500 whitespace-nowrap mr-4'>
+                    {data?.type === "free" ? (
+                      "Free"
+                    ) : (
+                      <span>Rp. {formatThousand(data?.price ?? 0)}</span>
+                    )}
+                  </h5>
+                  <a
+                    href={`${process.env.NEXT_PUBLIC_MEMBERPAGE_URL}/joined/${data.id}`}
+                    className='bg-orange-500 hover:bg-orange-400 transition-all duration-200 focus:outline-none shadow-inner text-white px-6 py-3 white whitespace-nowrap'
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                    {data?.type === "free" ? "Enroll Now" : "Buy Now"}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </CSSTransition>
+        </div>
+        <div className='w-3/4 mx-auto mt-8'>
+          <div className='w-3/4'>
+            <section>
+              <h6 className='font-medium text-gray-900 text-2xl mb-4'>
+                About <span className='text-teal-500'>Course</span>
+              </h6>
+              <p className='text-gray-600 text-lg leading-relaxed mb-3'>
+                {data?.description ?? "No Description Found"}
+              </p>
+            </section>
+
+            <section className='mt-10'>
+              <h6 className='font-medium text-gray-900 text-2xl mb-4'>
+                Course <span className='text-teal-500'>Photos</span>
+              </h6>
+              <div className='flex justify-start item-center -mx-4 mt-6'>
+                {data?.images?.length > 0 ? (
+                  data?.images?.map?.((photo, index) => (
+                    <CoursePhoto data={photo.image} key={index}></CoursePhoto>
+                  ))
+                ) : (
+                  <div className='w-full text-center py-12'>No Item Found</div>
+                )}
+              </div>
+            </section>
+          </div>
+        </div>
+      </section>
+      <section className='section-footer mt-24 py-12' ref={footer}>
+        <Footer></Footer>
       </section>
     </>
   );
