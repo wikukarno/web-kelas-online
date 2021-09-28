41386462b1387e575857d71963a75ddee4e1d4cc
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2021-09-29 00:06:28 +0700

    update folder Acordion/Item.js

--- src/components/Accordion/Item.js
+++ src/components/Accordion/Item.js
@@ -4,6 +4,11 @@ import { CSSTransition } from "react-transition-group";
 import ArrowDown from "public/images/icon-arrow-down.svg";
 
 export default function item({ name, id, child, active, toggle, children }) {
+  const [Height, setHeight] = useState(() => 0);
+  function calcHeight(e) {
+    console.log(e.offsetHeight);
+    setHeight(e.offsetHeight);
+  }
   return (
     <div className='bg-white border border-gray-300 overflow-hidden'>
       <div className='meta pl-4 py-2 relative flex justify-between item-center'>
@@ -19,6 +24,17 @@ export default function item({ name, id, child, active, toggle, children }) {
               ].join(" ")}></ArrowDown>
           )}
         </button>
+      </div>
+      <div
+        className='transition-all duration-500'
+        style={{ height: active === id ? Height : 0 }}>
+        <CSSTransition
+          in={active === id}
+          timeout={500}
+          onEnter={calcHeight}
+          className='accordion-item'>
+          <div className='accordion-item py-2 bg-gray-100'>{children}</div>
+        </CSSTransition>
       </div>
     </div>
   );
