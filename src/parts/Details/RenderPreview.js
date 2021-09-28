668efa43f4b7222f49fcda383f189ab8e8df81c4
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2021-09-28 21:39:11 +0700

    update file RenderPreview.js

--- src/parts/Details/RenderPreview.js
+++ src/parts/Details/RenderPreview.js
@@ -8,7 +8,55 @@ export default function RenderPreview({ previews }) {
       <Accordion>
         {(Active, toggle) => {
           return previews?.map((item, index) => {
-            <Item id={item.id}></Item>;
+            return (
+              <Item
+                id={item.id}
+                name={item.name}
+                child={item.lessons}
+                active={Active}
+                toggle={toggle}
+                key={`${index}-${item.id}`}>
+                {item?.lessons?.length > 0 &&
+                  item.lessons.map((child, index2) => (
+                    <div
+                      key={`${index2}-${child.id}`}
+                      className='relative flex justify-between items-center pl-8 pr-4 py-2'>
+                      <span className='text-gray-600'>
+                        {child?.name ?? "Course name"}
+                      </span>
+                      {index2 === 0 && (
+                        <Modal
+                          content={(toggleModal) => (
+                            <Youtube
+                              videoId={child?.video}
+                              id={child?.video}
+                              opts={{
+                                playerVars: {
+                                  autoplay: 1,
+                                  controls: 1,
+                                  showinfo: 1,
+                                },
+                              }}></Youtube>
+                          )}>
+                          {(toggleModal) => (
+                            <span
+                              className='link-wrapped'
+                              onClick={toggleModal}></span>
+                          )}
+                        </Modal>
+                      )}
+                      {index2 === 0 && (
+                        <IconPlay
+                          className='fill-teal-500'
+                          width={20}
+                          height={20}></IconPlay>
+                      )}
+
+                      {!child.isPreview && index !== 0 && <IconLock></IconLock>}
+                    </div>
+                  ))}
+              </Item>
+            );
           });
         }}
       </Accordion>
